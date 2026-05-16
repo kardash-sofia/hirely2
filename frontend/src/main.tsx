6f2829fb20335f69d31e875dcd2970d138f67751
kardash-sofia <sofiikardash@gmail.com>
2026-05-17 01:34:46 +0300

    [frontend] Make chat creation idempotent

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
@@ -6,10 +5,8 @@ import App from './App.tsx'
 import theme from './theme/index.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
 )
